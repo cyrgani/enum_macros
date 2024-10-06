3c6a576ff3dca5a88da0e4ed5dfbda84d868783f
cyrgani <ansgar.w.zielke@gmail.com>
2024-10-06 20:31:33 +0200

    change the behavior of `marker_type` regarding attributes on the original type

--- src/marker_type.rs
+++ src/marker_type.rs
@@ -1,8 +1,22 @@
 use proc_macro2::{Span, TokenStream};
 use quote::{format_ident, quote};
-use syn::{Fields, Generics, ItemEnum, Variant};
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
+use syn::{Fields, Generics, ItemEnum, Meta, Result, Token, Variant};
 
-pub fn marker_type(item: ItemEnum) -> TokenStream {
+pub struct Arguments {
+    attrs: Punctuated<Meta, Token![,]>,
+}
+
+impl Parse for Arguments {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(Self {
+            attrs: input.parse_terminated(Meta::parse, Token![,])?,
+        })
+    }
+}
+
+pub fn marker_type(args: Arguments, item: ItemEnum) -> TokenStream {
     let marker = ItemEnum {
         variants: item
             .variants
@@ -14,13 +28,25 @@ pub fn marker_type(item: ItemEnum) -> TokenStream {
             .collect(),
         ident: format_ident!("{}Marker", item.ident, span = Span::call_site()),
         generics: Generics::default(),
+        attrs: vec![],
         ..item.clone()
     };
+
+    let attrs = args
+        .attrs
+        .into_iter()
+        .map(|meta| {
+            quote! {
+                #[#meta]
+            }
+        })
+        .collect::<TokenStream>();
 
     quote! {
         #item
 
-        #[derive(Clone, Copy)]
+        #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+        #attrs
         #marker
     }
 }
